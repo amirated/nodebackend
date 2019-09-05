ef8d5baec4d4080c43e1093097203219e91b649c
Amitesh <amiteshr90@gmail.com>
2019-09-05 21:32:21 +0530

    restructuring to seperate the server and react files

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 var express = require("express");
 
 var app = express();
-
+var path = require('path');
 
 // app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", (req, res, next) => {
  // res.json(["Tony","Lisa","Michael","Ginger","Food"]);
- res.sendFile('index.html', {root: __dirname});
+ res.sendFile(path.resolve('./react_app/index.html'));
+ // res.sendFile('/../react_app/index.html', {root: __dirname});
 });
 
 
